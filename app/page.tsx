02185f8943ea4aa894578cf64e87ddc2de42d72d
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-04-23 09:50:20 +0100

    New commit message: add feq asked question section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Pricing from '@components/Pricing';
+import FrequentlyAskedQuestions from '@components/FrequentlyAskedQuestions';
 
 export default function Page() {
   return (
     <section className="flex flex-col gap-10">
       <Pricing />
+      <FrequentlyAskedQuestions />
     </section>
   );
 }
