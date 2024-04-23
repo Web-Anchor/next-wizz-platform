a09f1aa00e855aac8a189b1f33bbd67dbd69d400
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-04-23 09:54:40 +0100

    New commit message: add feature section component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Pricing from '@components/Pricing';
+import FeatureSections from '@components/FeatureSections';
 import FrequentlyAskedQuestions from '@components/FrequentlyAskedQuestions';
 
 export default function Page() {
   return (
     <section className="flex flex-col gap-10">
+      <FeatureSections />
       <Pricing />
       <FrequentlyAskedQuestions />
     </section>
