e59eb2e467b37758b1abe4b593ccce9048b781cd
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-04-24 18:48:47 +0100

    New commit message: add auth layout

--- app/components/AuthLayout.tsx
+++ app/components/AuthLayout.tsx
@@ -1,7 +1,8 @@
+import Breadcrumbs from '@components/Breadcrumbs';
 import Header from '@app/components/Header';
 import Footer from '@components/Footer';
 
-export default function Layout({
+export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -9,6 +10,7 @@ export default function Layout({
   return (
     <main className="flex min-h-screen max-w-7xl flex-col">
       <Header />
+      <Breadcrumbs />
       <section className="flex-auto pt-6 bg-slate-100">{children}</section>
       <Footer />
     </main>
