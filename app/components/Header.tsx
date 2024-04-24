64bf42e96e0cfa600ae778e895c782aab9940d2c
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-04-24 18:36:32 +0100

    New commit message: snags to auth routes

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -45,13 +45,13 @@ export default function Header() {
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    stroke-width="1.5"
+                    strokeWidth="1.5"
                     stroke="currentColor"
                     className="w-6 h-6"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25"
                     />
                   </svg>
@@ -77,7 +77,7 @@ export default function Header() {
                   </div>
                 )}
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className="flex flex-row gap-5 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 {!isSignedIn && homePath && (
                   <Link
                     href="/sign-in"
@@ -86,7 +86,7 @@ export default function Header() {
                     Sign in
                   </Link>
                 )}
-                {!isSignedIn && !homePath && (
+                {!homePath && (
                   <Link
                     href="/"
                     className="rounded-md bg-slate-800 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-slate-700"
@@ -94,7 +94,7 @@ export default function Header() {
                     Home
                   </Link>
                 )}
-                <UserButton />
+                <UserButton afterSignOutUrl="/" signInUrl="/sign-in" />
               </div>
             </div>
           </div>
