37129e851ccb5d6615a08bb102a45bf589cdc230
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-04-27 14:21:22 +0100

    New commit message: api route cleanup

--- app/api/v1/stripe/validate-key/route.ts
+++ app/api/v1/stripe/validate-key/route.ts
@@ -1,8 +1,6 @@
 import { auth } from '@clerk/nextjs/server';
 import { NextRequest, NextResponse } from 'next/server';
 
-const APP_URL = process.env.NEXT_PUBLIC_APP_URL;
-
 export async function POST(request: NextRequest) {
   auth().protect();
 
@@ -12,7 +10,6 @@ export async function POST(request: NextRequest) {
     // --------------------------------------------------------------------------------
     const body = await request.json();
     const key = body.key;
-    const { userId } = auth();
 
     const stripe = require('stripe')(key);
 
@@ -20,19 +17,6 @@ export async function POST(request: NextRequest) {
     // 📌  Validate API key by getting user charges
     // --------------------------------------------------------------------------------
     const charges = await stripe.charges.list({ limit: 1 });
-    // console.log('🔑 charges', charges);
-    console.log('🔑 charges', charges);
-
-    // if (charges?.error) {
-    //   return new Response(JSON.stringify({ error: charges?.error }), {
-    //     status: 401,
-    //   });
-    // }
-
-    // const { data } = await axios.get(
-    //   `${APP_URL}/api/v1/stripe/keys?account=${account}&keyId=${keyId}`
-    // );
-    // console.log('🔑 keys', data);
 
     return NextResponse.json({ charges });
   } catch (error: any) {
