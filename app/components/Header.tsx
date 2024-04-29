df5e80abd0659c12ec0e5b71703798efa1c4790d
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-04-29 21:56:20 +0100

    New commit message: snags to mobile screen layouts

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -115,83 +115,73 @@ export default function Header(props: Props) {
             {isSignedIn && (
               <div className="space-y-1 pb-4 pt-2">
                 {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
-                <Disclosure.Button
-                  as="a"
+                <Link
                   href="/dashboard"
                   className={path === '/dashboard' ? activeLink : link}
                 >
                   Dashboard
-                </Disclosure.Button>
-                <Disclosure.Button
-                  as="a"
+                </Link>
+                <Link
                   href="/dashboard/charges"
                   className={path === '/dashboard/charges' ? activeLink : link}
                 >
                   Charges
-                </Disclosure.Button>
-                <Disclosure.Button
-                  as="a"
+                </Link>
+                <Link
                   href="/dashboard/customers"
                   className={
                     path === '/dashboard/customers' ? activeLink : link
                   }
                 >
                   Customers
-                </Disclosure.Button>
-                <Disclosure.Button
-                  as="a"
+                </Link>
+                <Link
                   href="/dashboard/invoices"
                   className={path === '/dashboard/invoices' ? activeLink : link}
                 >
                   Invoices
-                </Disclosure.Button>
-                <Disclosure.Button
-                  as="a"
+                </Link>
+                <Link
                   href="/dashboard/stripe"
                   className={path === '/dashboard/stripe' ? activeLink : link}
                 >
                   Stripe
-                </Disclosure.Button>
-                <Disclosure.Button
-                  as="a"
+                </Link>
+                <Link
                   href="/dashboard/reports"
                   className={path === '/dashboard/reports' ? activeLink : link}
                 >
-                  Stripe
-                </Disclosure.Button>
-                <Disclosure.Button
-                  as="a"
+                  Reports
+                </Link>
+                <Link
                   href="/dashboard/profile"
                   className={path === '/dashboard/profile' ? activeLink : link}
                 >
                   Profile
-                </Disclosure.Button>
+                </Link>
               </div>
             )}
             {landingPath && (
               <div className="space-y-1 pb-4 pt-2">
                 {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
-                <Disclosure.Button
-                  as="a"
+                <Link
                   href="#"
                   className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
                 >
                   Dashboard
-                </Disclosure.Button>
-                <Disclosure.Button
-                  as="a"
-                  href="#"
+                </Link>
+                <Link
+                  href="#projects"
                   className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
                 >
                   Projects
-                </Disclosure.Button>
-                <Disclosure.Button
-                  as="a"
-                  href="#"
+                </Link>
+                <Link
+                  href="#questions"
                   className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
                 >
-                  Calendar
-                </Disclosure.Button>
+                  Questions
+                </Link>
               </div>
             )}
           </Disclosure.Panel>
