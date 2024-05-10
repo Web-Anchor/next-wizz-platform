0bd6837bab143173ccbd15c2dc68c3347270fe0b
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-05-10 19:27:54 +0100

    New commit message: add better workflow banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
+import Layout from '@components/Layout';
 import Pricing from '@components/Pricing';
 import FeatureSections from '@components/FeatureSections';
 import FrequentlyAskedQuestions from '@components/FrequentlyAskedQuestions';
-import Layout from '@components/Layout';
+import Workflow from '@components/Workflow';
 
 export default function Page() {
   return (
     <Layout>
       <FeatureSections />
       <Pricing />
+      <Workflow />
       <FrequentlyAskedQuestions />
     </Layout>
   );
