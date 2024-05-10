5133aaac2dfaf1beb578a2224659966a57011b38
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-05-10 19:00:12 +0100

    New commit message: add Q&A content & snags

--- app/components/FrequentlyAskedQuestions.tsx
+++ app/components/FrequentlyAskedQuestions.tsx
@@ -6,9 +6,50 @@ import { usePathname } from 'next/navigation';
 
 const faqs = [
   {
-    question: "What's the best thing about Switzerland?",
+    question: 'How can I connect my Stripe account to the app?',
     answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      'Connecting your Stripe account is simple! Just go to the settings menu, select the payment integration option, and follow the prompts to link your account via API.',
+  },
+  {
+    question: 'Can I customize the invoice templates?',
+    answer:
+      'Absolutely! Our app offers customizable templates to help you personalize your invoices and showcase your brand identity.',
+  },
+  {
+    question: 'How do I access and download my invoices for self-printing?',
+    answer:
+      'You can easily access and download your invoices by logging into your account and navigating to the invoices section. From there, you can view and download any invoice you need.',
+  },
+  {
+    question: 'Are payments processed securely through the app?',
+    answer:
+      'Yes, we ensure secure payment processing by integrating with Stripe`s reliable payment system, providing a safe and trustworthy transaction environment.',
+  },
+  {
+    question: 'Can I set up automated reminders for overdue payments?',
+    answer:
+      'Yes, our app allows you to configure automated reminders for outstanding invoices, helping you stay on top of payments and improve your cash flow.',
+  },
+  {
+    question: 'What kind of reporting features are available in the app?',
+    answer:
+      'Our app offers detailed reporting features that provide insights into your invoicing activities, allowing you to track payments, pending invoices, and overall financial performance effortlessly.',
+  },
+  {
+    question:
+      'Do higher-tier plans include all the features of lower-tier plans?',
+    answer:
+      'Yes, each subsequent plan includes all the features of the previous plans, ensuring that you have access to a cumulative set of benefits as you upgrade your subscription.',
+  },
+  {
+    question: 'Is customer support available for all plans?',
+    answer:
+      'Yes, customer support is available for all our users. Enterprise plan subscribers, however, receive priority customer support for any queries or assistance they may need.',
+  },
+  {
+    question: 'How can I upgrade or downgrade my subscription plan?',
+    answer:
+      'You can easily manage your subscription plan from your account settings. Simply select the plan you wish to switch to, and our system will guide you through the process seamlessly.',
   },
   // More questions...
 ];
@@ -24,7 +65,7 @@ export default function FrequentlyAskedQuestions() {
             Frequently asked questions
           </h2>
           <dl className="mt-10 space-y-6 divide-y divide-gray-900/10">
-            {faqs.map((faq) => (
+            {faqs?.map((faq) => (
               <Disclosure as="div" key={faq.question} className="pt-6">
                 {({ open }) => (
                   <>
