4102ce5e6e9fe1dbc67d4b02a0c2a56c28295e5a
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-05-13 10:13:20 +0100

    New commit message: change logon btn place & move to landing /main screen

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -141,14 +141,6 @@ export default function Header(props: Props) {
                             </div>
                           </div>
                         )}
-                        {!isSignedIn && landingPath && isLoaded && (
-                          <Link
-                            href="/sign-in"
-                            className="rounded-md bg-slate-800 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-slate-700"
-                          >
-                            Sign in
-                          </Link>
-                        )}
                       </Menu.Button>
                     </div>
                     <Transition
@@ -191,8 +183,17 @@ export default function Header(props: Props) {
                     </Transition>
                   </Menu>
                 </div>
-                <div className="-mr-2 flex items-center sm:hidden">
+                <div className="-mr-2 flex flex-row gap-2 items-center sm:hidden">
                   {/* Mobile menu button */}
+                  {!isSignedIn && landingPath && isLoaded && (
+                    <Link
+                      href="/sign-in"
+                      className="rounded-md bg-slate-800 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-slate-700"
+                    >
+                      Sign in
+                    </Link>
+                  )}
+
                   <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span className="absolute -inset-0.5" />
                     <span className="sr-only">Open main menu</span>
