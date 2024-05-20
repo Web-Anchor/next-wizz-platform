36d9f0a039fc20c70020fd6d077dd170a1951629
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-05-20 10:26:30 +0100

    New commit message: refactor invoicing page & add new form

--- app/components/InvoicingCard.tsx
+++ app/components/InvoicingCard.tsx
@@ -1,80 +1,108 @@
-import { Switch as SwitchUI } from '@headlessui/react';
 import Switch from './Switch';
-import { memo, useState } from 'react';
+import { useState } from 'react';
 import { cFetch } from '@lib/cFetcher';
 import { mutate } from 'swr';
 import { toast } from 'sonner';
 import Button from './Button';
 
-const table = [
-  {
+const table = {
+  header: {
     title: 'Invoice Header',
     description:
       'Invoice header text is the text that appears at the top of the page.',
     stateKey: 'header',
     maxLength: 100,
   },
-  {
+  memo: {
     title: 'Memo',
     description:
       'Memo text is the text that appears above the invoice pricing section.',
     stateKey: 'memo',
     maxLength: 100,
   },
-  {
+  footer: {
     title: 'Footer',
     description:
       'Footer text is the text that appears at the bottom of the invoice.',
     stateKey: 'footer',
     maxLength: 200,
   },
-  {
+  customFields: {
     title: 'Custom Fields',
     description:
       'Custom fields are additional fields you can add to your invoice.',
     stateKey: 'customFields',
     maxLength: 20,
   },
-];
+};
 
 type CustomField = {
   name: string;
   value: string;
 };
-type Form = {
-  [key: string]: string;
+
+const Section = ({
+  children,
+  hidden,
+}: {
+  children: React.ReactNode;
+  hidden?: boolean;
+}) => {
+  if (hidden) return null;
+  return <section className="flex flex-col gap-5">{children}</section>;
 };
 
 export default function InvoiceTable() {
   const [state, setState] = useState<{
-    [key: string]: boolean | CustomField[] | undefined;
+    isHeader?: boolean;
+    isMemo?: boolean;
+    isFooter?: boolean;
+    isCustomFields?: boolean;
+    fetching?: boolean;
+    customFields: CustomField[];
+    header?: string;
+    memo?: string;
+    footer?: string;
   }>({
-    fields: [{ name: '', value: '' }],
+    header: '',
+    memo: '',
+    footer: '',
+    customFields: [{ name: '', value: '' }],
   });
 
-  function setEnabled(key: string, value?: boolean) {
-    setState((prev) => ({ ...prev, [key]: value }));
+  const handleChange = (index: number, key: string, value: string) => {
+    const updatedFields = [...state.customFields];
+    (updatedFields as any)[index][key] = value;
+    setState({ ...state, customFields: updatedFields });
+  };
+
+  const addCustomField = () => {
+    setState({
+      ...state,
+      customFields: [...state.customFields, { name: '', value: '' }],
+    });
+  };
+
+  function removeCustomField(index: number) {
+    setState({
+      ...state,
+      customFields: state.customFields.filter((_, i) => i !== index),
+    });
   }
 
-  async function submit(form: any) {
+  async function submit(e: React.FormEvent) {
     try {
+      e.preventDefault();
+      return console.log(state);
       // --------------------------------------------------------------------------------
       // 📌  Add Stripe API key to db
       // --------------------------------------------------------------------------------
       setState((prev) => ({ ...prev, fetching: true }));
-      const header = form.get('header');
-      const footer = form.get('footer');
-      const memo = form.get('memo');
-      const customFields = Array.from(form.entries()).filter(([key]: any) =>
-        key.includes('custom-field')
-      ) as [string, string][];
-
-      return console.log(form.entries(), header, footer, customFields);
 
       const { data, status } = await cFetch({
         url: '/api/v1/templates/add',
         method: 'POST',
-        data: { header },
+        // data: { header },
       });
 
       if (status !== 200 || data?.error) {
@@ -92,135 +120,168 @@ export default function InvoiceTable() {
     }
   }
 
-  const CustomFields = memo(function CustomFields() {
-    const [cState, cSetState] = useState<{
-      [key: string]: boolean | CustomField[] | undefined;
-    }>({
-      fields: [{ name: '', value: '' }],
-    });
-    const config = table.filter(
-      (config) => config.stateKey === 'customFields'
-    )[0];
-    console.log('cState', cState);
+  return (
+    <form onSubmit={submit} className="flex flex-col gap-5">
+      <Section>
+        <div className="flex flex-row gap-5 justify-between">
+          <label>
+            {table?.header?.title}
+            <span className="text-xs ml-1 text-gray-400">
+              {table?.header?.maxLength} characters max
+            </span>
+          </label>
 
-    function addCustomField() {
-      cSetState((prev) => ({
-        ...prev,
-        fields: [...(prev?.fields as CustomField[]), { name: '', value: '' }],
-      }));
-    }
+          <Switch
+            enabled={state?.isHeader}
+            onChange={() =>
+              setState((prev) => ({
+                ...prev,
+                isHeader: !prev.isHeader,
+              }))
+            }
+          />
+        </div>
+        <Section>
+          <p className="text-xs text-gray-500">{table?.header?.description}</p>
+        </Section>
+        <Section hidden={!state?.isHeader}>
+          <textarea
+            placeholder="Memo"
+            value={state.header}
+            onChange={(e) => setState({ ...state, header: e.target.value })}
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </Section>
+      </Section>
+      <Section>
+        <div className="flex flex-row gap-5 justify-between">
+          <label>
+            {table?.footer?.title}
+            <span className="text-xs ml-1 text-gray-400">
+              {table?.footer?.maxLength} characters max
+            </span>
+          </label>
 
-    function removeCustomField(index: number) {
-      console.log('removeCustomField', index);
+          <Switch
+            enabled={state?.isFooter}
+            onChange={() =>
+              setState((prev) => ({
+                ...prev,
+                isFooter: !prev.isFooter,
+              }))
+            }
+          />
+        </div>
+        <Section>
+          <p className="text-xs text-gray-500">{table?.footer?.description}</p>
+        </Section>
+        <Section hidden={!state?.isFooter}>
+          <textarea
+            placeholder="Memo"
+            value={state.footer}
+            onChange={(e) => setState({ ...state, footer: e.target.value })}
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </Section>
+      </Section>
+      <Section>
+        <div className="flex flex-row gap-5 justify-between">
+          <label>
+            {table?.memo?.title}
+            <span className="text-xs ml-1 text-gray-400">
+              {table?.memo?.maxLength} characters max
+            </span>
+          </label>
 
-      cSetState((prev) => ({
-        ...prev,
-        fields: (prev?.fields as CustomField[])?.filter((_, i) => i !== index),
-      }));
-    }
+          <Switch
+            enabled={state?.isMemo}
+            onChange={() =>
+              setState((prev) => ({
+                ...prev,
+                isMemo: !prev.isMemo,
+              }))
+            }
+          />
+        </div>
+        <Section>
+          <p className="text-xs text-gray-500">{table?.memo?.description}</p>
+        </Section>
+        <Section hidden={!state?.isMemo}>
+          <textarea
+            placeholder="Memo"
+            value={state.memo}
+            onChange={(e) => setState({ ...state, memo: e.target.value })}
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </Section>
+      </Section>
 
-    function updateCustomField(index: number, key: string, value: string) {
-      cSetState((prev) => ({
-        ...prev,
-        fields: (prev?.fields as CustomField[])?.map((field, i) =>
-          i === index ? { ...field, [key]: value } : field
-        ),
-      }));
-    }
+      <Section>
+        <div className="flex flex-row gap-5 justify-between">
+          <label>
+            {table?.customFields?.title}
+            <span className="text-xs ml-1 text-gray-400">
+              {table?.customFields?.maxLength} characters max
+            </span>
+          </label>
 
-    return (
-      <div className="flex flex-col gap-5 mt-5">
-        {(cState?.fields as CustomField[])?.map((row, key) => (
-          <div key={key} className="flex flex-row gap-5 items-center">
+          <Switch
+            enabled={state?.isCustomFields}
+            onChange={() =>
+              setState((prev) => ({
+                ...prev,
+                isCustomFields: !prev.isCustomFields,
+                customFields: prev.customFields
+                  ? [{ name: '', value: '' }]
+                  : prev.customFields,
+              }))
+            }
+          />
+        </div>
+        <Section>
+          <p className="text-xs text-gray-500">
+            {table?.customFields?.description}
+          </p>
+        </Section>
+        <Section hidden={!state?.isCustomFields}>
+          {state?.customFields?.map((field, index) => (
+            <div key={index} className="flex flex-row gap-5 items-center">
+              <input
+                type="text"
+                placeholder="Name"
+                value={field.name}
+                onChange={(e) => handleChange(index, 'name', e.target.value)}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+              <input
+                type="text"
+                placeholder="Value"
+                value={field.value}
+                onChange={(e) => handleChange(index, 'value', e.target.value)}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+              <Button
+                title="Remove"
+                style="ghost"
+                hide={state?.customFields?.length === 1}
+                onClick={() => removeCustomField(index)}
+              />
+            </div>
+          ))}
+          <div className="card-actions justify-start">
             <Button
-              title="Remove"
+              title="Add custom field"
               style="ghost"
-              onClick={() => removeCustomField(key)}
-            />
-            <input
-              type="text"
-              name={`custom-field-name-${key}`}
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              placeholder="Field name (e.g. PO Number)"
-              maxLength={config?.maxLength!}
-              // value={(state?.fields?.[key]?.name as string) ?? ''}
-              // onChange={(e) => updateCustomField(key, 'name', e.target.value)}
-            />
-            <input
-              type="text"
-              name={`custom-field-value-${key}`}
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              placeholder="Field value"
-              maxLength={config?.maxLength!}
-              // value={(state?.fields?.[key]?.value as string) ?? ''}
-              // onChange={(e) => updateCustomField(key, 'value', e.target.value)}
+              onClick={addCustomField}
             />
           </div>
-        ))}
-        <Button title="Add" style="ghost" onClick={addCustomField} />
-      </div>
-    );
-  });
+        </Section>
+      </Section>
 
-  return (
-    <section>
-      <div className="card max-w-4xl bg-base-100 shadow-xl">
-        <div className="card-body">
-          <h2 className="card-title">Customize your invoice feel & look</h2>
-          <form action={submit} className="flex flex-col gap-5">
-            {table?.map((item, key) => {
-              const isCustomFields = item?.stateKey === 'customFields';
-
-              return (
-                <SwitchUI.Group
-                  key={key}
-                  as="div"
-                  className="flex justify-between"
-                >
-                  <span className="flex flex-grow flex-col pr-14">
-                    <SwitchUI.Label
-                      as="span"
-                      className="text-sm font-medium leading-6 text-gray-900"
-                      passive
-                    >
-                      {item.title}{' '}
-                      <span className="text-xs text-gray-400">
-                        {item.maxLength} characters max
-                      </span>
-                    </SwitchUI.Label>
-                    <SwitchUI.Description
-                      as="span"
-                      className="text-xs text-gray-500"
-                    >
-                      {!state?.[item?.stateKey] && <p>{item.description}</p>}
-                      {state?.[item?.stateKey] && !isCustomFields && (
-                        <input
-                          type="text"
-                          name={item.stateKey}
-                          className="block w-full rounded-md border-0 py-1.5 mt-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                          placeholder={item?.title}
-                          maxLength={item?.maxLength}
-                        />
-                      )}
-                      {state?.[item?.stateKey] && isCustomFields && (
-                        <CustomFields />
-                      )}
-                    </SwitchUI.Description>
-                  </span>
-                  <Switch
-                    enabled={state?.[item?.stateKey] as boolean}
-                    onChange={(value) => setEnabled(item?.stateKey, value)}
-                  />
-                </SwitchUI.Group>
-              );
-            })}
-            <div className="card-actions justify-end">
-              <Button title="Save" type="submit" />
-              <Button title="Cancel" style="secondary" />
-            </div>
-          </form>
-        </div>
+      <div className="card-actions justify-end">
+        <Button title="Submit" type="submit" />
+        <Button title="Cancel" style="secondary" />
       </div>
-    </section>
+    </form>
   );
 }
