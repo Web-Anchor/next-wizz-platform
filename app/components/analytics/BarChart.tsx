0e8939a5fd2b018b41a0243d6e81f012e8abf3d2
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-05-24 22:48:51 +0100

    New commit message: add css snags to wrapper

--- app/components/analytics/BarChart.tsx
+++ app/components/analytics/BarChart.tsx
@@ -42,7 +42,7 @@ export default function BarChart(props: Props): React.ReactElement | null {
   if (!props?.data?.length) return null;
 
   return (
-    <section className={props.class}>
+    <section className={classNames('relative', props.class)}>
       <div
         className={classNames(
           'flex lg:hidden absolute left-0 top-0 z-10 items-center justify-center w-[360px] h-[360px]',
--- app/components/analytics/RadialBar.tsx
+++ app/components/analytics/RadialBar.tsx
@@ -53,7 +53,7 @@ export default function RadialBar(props: Props): React.ReactElement | null {
   if (!props?.data?.length) return null;
 
   return (
-    <section className={props.class}>
+    <section className={classNames('relative', props.class)}>
       <div
         className={classNames(
           'flex lg:hidden absolute left-0 top-0 z-10 items-center justify-center w-[360px] h-[360px]',
