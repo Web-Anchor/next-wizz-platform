c7e899caa55e0c81af94d236c0d514da46d36ac6
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-06-15 14:05:08 +0100

    New commit message: add notifications to breadcrumb componant

--- app/components/Breadcrumbs.tsx
+++ app/components/Breadcrumbs.tsx
@@ -4,6 +4,7 @@ import { ChevronRightIcon, HomeIcon } from '@heroicons/react/20/solid';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { classNames } from '@helpers/index';
+import Notifications from './Notifications';
 
 type Props = {
   class?: string;
@@ -29,36 +30,43 @@ export default function Breadcrumbs(props: Props) {
       )}
       aria-label="Breadcrumb"
     >
-      <ol role="list" className="flex items-center space-x-4">
-        <li>
-          <div>
-            <Link
-              href="/dashboard"
-              className="text-gray-400 hover:text-gray-500"
-            >
-              <HomeIcon className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
-              <span className="sr-only">Home</span>
-            </Link>
-          </div>
-        </li>
-        {pages.map((page) => (
-          <li key={page.name}>
-            <div className="flex items-center">
-              <ChevronRightIcon
-                className="h-5 w-5 flex-shrink-0 text-gray-400"
-                aria-hidden="true"
-              />
+      <section className="flex flex-row justify-between">
+        <ol role="list" className="flex items-center space-x-4">
+          <li>
+            <div>
               <Link
-                href={page.href}
-                className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
-                aria-current={page.current ? 'page' : undefined}
+                href="/dashboard"
+                className="text-gray-400 hover:text-gray-500"
               >
-                {page.name}
+                <HomeIcon
+                  className="h-5 w-5 flex-shrink-0"
+                  aria-hidden="true"
+                />
+                <span className="sr-only">Home</span>
               </Link>
             </div>
           </li>
-        ))}
-      </ol>
+          {pages.map((page) => (
+            <li key={page.name}>
+              <div className="flex items-center">
+                <ChevronRightIcon
+                  className="h-5 w-5 flex-shrink-0 text-gray-400"
+                  aria-hidden="true"
+                />
+                <Link
+                  href={page.href}
+                  className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                  aria-current={page.current ? 'page' : undefined}
+                >
+                  {page.name}
+                </Link>
+              </div>
+            </li>
+          ))}
+        </ol>
+
+        <Notifications />
+      </section>
     </nav>
   );
 }
