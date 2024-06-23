a85ab8e2b55a5c9a23b1b570e8fc7b0c64e0d65a
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-06-23 16:11:49 +0100

    New commit message: remove css media q from invoice prev page

--- app/dashboard/invoices/template-preview/controller.tsx
+++ app/dashboard/invoices/template-preview/controller.tsx
@@ -12,15 +12,13 @@ export default function Page() {
   const { html, isLoading } = useBuildTemplate({ id });
 
   //  Minimum width = 1200 pixels * 0.707 ≈ 848 pixels
-  const aimHeight = 1200;
-  const aimWidth = Math.floor(aimHeight * 0.707);
 
   return (
     <Wrapper>
       <Spinner hidden={!isLoading} />
       {!isLoading && html && (
         <div
-          className={`w-full h-full max-w-4xl mx-auto p-4 min-h-[1200px] min-w-[848px] lg:min-h-[${aimHeight}px] lg:min-w-[${aimWidth}px]`}
+          className={`w-full h-full max-w-4xl mx-auto p-4 min-h-[1200px] min-w-[848px]`}
           dangerouslySetInnerHTML={{ __html: html }}
         />
       )}
