a7f5a869e2a52d04b6fc0707d99f9e642571982f
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-06-24 19:40:24 +0100

    New commit message: add onblur event switch to input for cutom fields

--- app/dashboard/invoices/controller.tsx
+++ app/dashboard/invoices/controller.tsx
@@ -413,7 +413,8 @@ export default function Page() {
                                 type="text"
                                 placeholder="Define a custom field value"
                                 defaultValue={value?.value}
-                                onChange={(e) => {
+                                onBlur={(e) => {
+                                  console.log('🔍 Blur event', e.target.value);
                                   debouncedHandleChange(cKey, e.target.value);
                                 }}
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
