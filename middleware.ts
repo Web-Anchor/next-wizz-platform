8a918f2001a0445112886cdc8dab5ca0e363f7fe
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-07-02 19:49:29 +0100

    New commit message: changes to middleware api & restructure

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,19 @@
-import { authMiddleware } from '@clerk/nextjs';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-// See https://clerk.com/docs/references/nextjs/auth-middleware
-// for more information about configuring your Middleware
-export default authMiddleware({
-  // Allow signed out users to access the specified routes:
-  publicRoutes: ['/', '/sign-up', '/sign-in', '/contact'],
-  ignoredRoutes: [
-    '/((?!api|trpc))(_next.*|.+.[w]+$)',
-    '/api/v1/testimonials',
-    '/api/v1/templates/template',
-    '/api/v1/support/add-ticket-public',
-    '/api/v1/re-captcha-validate',
-  ],
-  signInUrl: '/sign-in',
-  apiRoutes: ['/api/(.)', '/trpc/(.)'],
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+  '/contact(.*)',
+  '/api/(.*)',
+  '/trpc/(.*)',
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
 });
 
 export const config = {
-  matcher: [
-    // Exclude files with a "." followed by an extension, which are typically static files.
-    // Exclude files in the _next directory, which are Next.js internals.
-    '/((?!.+\\.[\\w]+$|_next).*)',
-    // Re-include any files in the api or trpc folders that might have an extension
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
