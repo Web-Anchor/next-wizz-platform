831962e376f91f3b8afbdf107b7cb84b851ecd82
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-07-04 11:19:56 +0100

    New commit message: add route guards & protection agains unsubscribed users

--- app/dashboard/subscriptions/page.tsx
+++ app/dashboard/subscriptions/page.tsx
@@ -1,7 +1,10 @@
+import { subRouteGuard } from '@server/route-protection';
 import Controller from './controller';
 import AuthLayout from '@components/AuthLayout';
 
 export default async function Page() {
+  await subRouteGuard();
+
   return (
     <AuthLayout>
       <Controller />
