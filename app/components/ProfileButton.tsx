0f181acaae2a25823d16578eba93b73a4827183a
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-07-04 08:41:56 +0100

    New commit message: snags to pop ups & ui enhance

--- app/components/ProfileButton.tsx
+++ app/components/ProfileButton.tsx
@@ -1,18 +1,12 @@
 'use client';
 
-import { Fragment } from 'react';
+import { useState } from 'react';
 import { classNames } from '@helpers/index';
-import {
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-  Transition,
-} from '@headlessui/react';
 import UserProfileCard from './UserProfileCard';
 import { useClerk, useUser } from '@clerk/nextjs';
 import { useRouter, useSearchParams } from 'next/navigation';
 import Link from 'next/link';
+import { XCircleIcon } from '@heroicons/react/24/outline';
 import { fakerUser } from '@lib/faker';
 
 type Props = {
@@ -20,6 +14,7 @@ type Props = {
 };
 
 export default function ProfileButton(props: Props): React.ReactElement {
+  const [state, setState] = useState<{ open?: boolean }>({});
   let { isSignedIn, user, isLoaded } = useUser();
   // user = fakerUser(true); // faker data
   const { signOut } = useClerk();
@@ -32,71 +27,67 @@ export default function ProfileButton(props: Props): React.ReactElement {
     // --------------------------------------------------------------------------------
     // 📌 Sign Out User from current session
     // --------------------------------------------------------------------------------
-    signOut(() => router.push(`/sign-in?id=${id}`));
+    signOut(() => router.push(`/sign-in` + id && `?id=${id}`));
   }
 
   return (
-    <Menu as="div" className={classNames('relative ml-3')}>
-      <div>
-        <MenuButton className="relative flex rounded-full text-sm">
-          {isSignedIn && (
-            <UserProfileCard
-              imgSrc={user?.imageUrl}
-              firstName={user?.firstName as string}
-              order="right"
-              disabled
-            />
-          )}
-          {!isLoaded && (
-            <div className="flex flex-col gap-4 w-fit">
-              <div className="flex gap-4 items-center">
-                <div className="flex flex-col gap-2">
-                  <div className="skeleton h-4 w-8"></div>
-                </div>
-                <div className="skeleton w-10 h-10 rounded-full shrink-0"></div>
-              </div>
-            </div>
-          )}
-        </MenuButton>
-      </div>
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-200"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
+    <section className={classNames('relative ml-3')}>
+      <div
+        onClick={() => setState((prev) => ({ ...prev, open: !prev.open }))}
+        className="relative cursor-pointer"
       >
-        <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white p-2 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <MenuItem>
-            {({ active }) => (
-              <Link
-                href="/dashboard"
-                className={classNames(
-                  active ? 'bg-gray-100' : '',
-                  'block px-4 py-2 text-sm text-gray-700'
-                )}
-              >
-                Dashboard
-              </Link>
-            )}
-          </MenuItem>
-          <MenuItem>
-            {({ active }) => (
-              <button
-                onClick={signOutUser}
-                className={classNames(
-                  'block px-4 py-2 text-sm text-gray-700 w-[100%] text-start',
-                  active && 'bg-gray-100'
-                )}
-              >
-                <p>Sign out</p>
-              </button>
-            )}
-          </MenuItem>
-        </MenuItems>
-      </Transition>
-    </Menu>
+        {isSignedIn && (
+          <UserProfileCard
+            imgSrc={user?.imageUrl}
+            firstName={user?.firstName as string}
+            order="right"
+            disabled
+          />
+        )}
+        {!isLoaded && (
+          <div className="flex flex-col gap-4 w-fit">
+            <div className="flex gap-4 items-center">
+              <div className="flex flex-col gap-2">
+                <div className="skeleton h-4 w-8"></div>
+              </div>
+              <div className="skeleton w-10 h-10 rounded-full shrink-0"></div>
+            </div>
+          </div>
+        )}
+      </div>
+
+      <div
+        className={classNames(
+          'absolute shadow-md top-14 min-w-[200px] right-0 rounded-xl bg-white p-4 ring-1 ring-indigo-600',
+          !state?.open && 'hidden'
+        )}
+      >
+        <div className="relative flex flex-col gap-3">
+          <Link
+            href="/dashboard"
+            className="bg-transparent inline-flex items-center border-b-2 border-transparent px-1 text-sm font-medium text-gray-500 hover:text-gray-700 hover:bg-transparent shadow-none"
+          >
+            Dashboard
+          </Link>
+          <section className={classNames('border-t pt-2')}>
+            <div
+              className="cursor-pointer bg-transparent inline-flex items-center border-b-2 border-transparent px-1 text-sm font-medium text-gray-500 hover:text-gray-700 hover:bg-transparent shadow-none"
+              onClick={signOutUser}
+            >
+              Sign out
+            </div>
+          </section>
+          <div
+            className="absolute -top-3 -right-3 flex-shrink-0"
+            onClick={() => setState((prev) => ({ ...prev, open: false }))}
+          >
+            <XCircleIcon
+              className="cursor-pointer h-5 w-5 text-gray-600"
+              aria-hidden="true"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
--- app/components/UserNotification.tsx
+++ app/components/UserNotification.tsx
@@ -26,40 +26,38 @@ export default function UserNotification(props: Props) {
   }
 
   return (
-    <>
-      <div
-        className={classNames(
-          'absolute shadow-md top-14 left-[100vw] min-w-[300px] left-popup-container rounded-xl bg-white p-4',
-          !props?.open && 'hidden'
-        )}
-      >
-        <div className="relative flex flex-col gap-3">
-          <Logo />
-          <Link
-            href="/"
-            className="bg-transparent inline-flex items-center border-b-2 border-transparent px-1 text-sm font-medium text-gray-500 hover:text-gray-700 hover:bg-transparent shadow-none"
-          >
-            Dashboard
-          </Link>
-          <section className={classNames('border-t pt-2')}>
-            <div
-              className="cursor-pointer bg-transparent inline-flex items-center border-b-2 border-transparent px-1 text-sm font-medium text-gray-500 hover:text-gray-700 hover:bg-transparent shadow-none"
-              onClick={signOutUser}
-            >
-              Sign out
-            </div>
-          </section>
+    <div
+      className={classNames(
+        'absolute shadow-md top-14 left-[100vw] min-w-[300px] left-popup-container rounded-xl bg-white p-4 ring-1 ring-indigo-600',
+        !props?.open && 'hidden'
+      )}
+    >
+      <div className="relative flex flex-col gap-3">
+        <Logo />
+        <Link
+          href="/"
+          className="bg-transparent inline-flex items-center border-b-2 border-transparent px-1 text-sm font-medium text-gray-500 hover:text-gray-700 hover:bg-transparent shadow-none"
+        >
+          Dashboard
+        </Link>
+        <section className={classNames('border-t pt-2')}>
           <div
-            className="absolute top-0 right-0 flex-shrink-0"
-            onClick={() => props?.setter?.(false)}
+            className="cursor-pointer bg-transparent inline-flex items-center border-b-2 border-transparent px-1 text-sm font-medium text-gray-500 hover:text-gray-700 hover:bg-transparent shadow-none"
+            onClick={signOutUser}
           >
-            <XCircleIcon
-              className="cursor-pointer h-5 w-5 text-gray-600"
-              aria-hidden="true"
-            />
+            Sign out
           </div>
+        </section>
+        <div
+          className="absolute top-0 right-0 flex-shrink-0"
+          onClick={() => props?.setter?.(false)}
+        >
+          <XCircleIcon
+            className="cursor-pointer h-5 w-5 text-gray-600"
+            aria-hidden="true"
+          />
         </div>
       </div>
-    </>
+    </div>
   );
 }
