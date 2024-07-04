f2071651346d7a224e17ac9c94faa24f6cc76576
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-07-04 09:06:12 +0100

    New commit message: redirect path change

--- app/components/UserNotification.tsx
+++ app/components/UserNotification.tsx
@@ -5,7 +5,7 @@ import { useClerk } from '@clerk/nextjs';
 import { useRouter } from 'next/navigation';
 import { XCircleIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import Logo from './Logo';
+import Logo from '@components/Logo';
 
 type Props = {
   open?: boolean;
@@ -35,7 +35,7 @@ export default function UserNotification(props: Props) {
       <div className="relative flex flex-col gap-3">
         <Logo />
         <Link
-          href="/"
+          href="/dashboard"
           className="bg-transparent inline-flex items-center border-b-2 border-transparent px-1 text-sm font-medium text-gray-500 hover:text-gray-700 hover:bg-transparent shadow-none"
         >
           Dashboard
