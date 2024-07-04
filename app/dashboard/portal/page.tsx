a61fcc5846e3b4dcf7c486205e60b178c71ddcab
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-07-04 09:53:00 +0100

    New commit message: extend & add route protections to other paths

--- app/dashboard/portal/page.tsx
+++ app/dashboard/portal/page.tsx
@@ -1,7 +1,10 @@
+import { advancedSubRouteGuard } from '@server/route-protection';
 import Controller from './controller';
 import AuthLayout from '@components/AuthLayout';
 
 export default async function Page() {
+  await advancedSubRouteGuard();
+
   return (
     <AuthLayout>
       <Controller />
