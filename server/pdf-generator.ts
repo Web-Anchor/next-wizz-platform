a0d2f51eb192f258034ffaa9eddc20c52809a57e
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-07-11 12:59:42 +0100

    New commit message: switch to puppeteer extra

--- server/pdf-generator.ts
+++ server/pdf-generator.ts
@@ -1,6 +1,7 @@
 'use server';
 
-import puppeteer from 'puppeteer-core';
+// @ts-ignore
+import puppeteer from 'puppeteer-extra';
 import chromium from '@sparticuz/chromium';
 
 type FetcherTypes = {
@@ -14,36 +15,48 @@ export async function pdfToBase64(
     // --------------------------------------------------------------------------------
     // 📌  Puppeteer
     // --------------------------------------------------------------------------------
-    console.log('📝 Creating a new page...');
-    const browser = await puppeteer.launch({
-      args: process.env.CHROME_EXECUTABLE_PATH ? undefined : chromium.args, // 🚧 locally throws error
-      defaultViewport: chromium.defaultViewport,
-      executablePath:
-        process.env.CHROME_EXECUTABLE_PATH ||
-        (await chromium.executablePath(
-          '/var/task/node_modules/@sparticuz/chromium/bin'
-        )),
-    });
-    console.log('Browser created...');
-    const page = await browser.newPage();
-    console.log('📝 Page created...');
+    // console.log('📝 Creating a new page...');
+    // const browser = await puppeteer.launch({
+    //   args: process.env.CHROME_EXECUTABLE_PATH ? undefined : chromium.args, // 🚧 locally throws error
+    //   defaultViewport: chromium.defaultViewport,
+    //   executablePath:
+    //     process.env.CHROME_EXECUTABLE_PATH ||
+    //     (await chromium.executablePath(
+    //       '/var/task/node_modules/@sparticuz/chromium/bin'
+    //     )),
+    // });
+    // console.log('Browser created...');
+    // const page = await browser.newPage();
+    // console.log('📝 Page created...');
 
-    // --------------------------------------------------------------------------------
-    // 📌 Set the HTML content of the page
-    // page.goto with a data: URL, Puppeteer will trigger network requests to load external resources like images, scripts, and stylesheets
-    // --------------------------------------------------------------------------------
-    await page.goto(`data:text/html,${props.html}`, {
-      waitUntil: 'networkidle0',
-      timeout: 5000,
-    });
+    // // --------------------------------------------------------------------------------
+    // // 📌 Set the HTML content of the page
+    // // page.goto with a data: URL, Puppeteer will trigger network requests to load external resources like images, scripts, and stylesheets
+    // // --------------------------------------------------------------------------------
+    // await page.goto(`data:text/html,${props.html}`, {
+    //   waitUntil: 'networkidle0',
+    //   timeout: 5000,
+    // });
 
-    console.log('📝 Page loaded...');
-    const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true }); // Set the PDF format
-    await browser.close();
+    // console.log('📝 Page loaded...');
+    // const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true }); // Set the PDF format
+    // await browser.close();
 
     // --------------------------------------------------------------------------------
     // 📌 Convert the PDF buffer to base64
     // --------------------------------------------------------------------------------
+    // const base64PDF = pdfBuffer.toString('base64');
+
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      executablePath:
+        process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath()),
+    });
+    const page = await browser.newPage();
+    await page.setContent(props.html);
+    const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
+    await browser.close();
     const base64PDF = pdfBuffer.toString('base64');
 
     return { base64PDF };
